a2edc9f024500eb6b7555b2ab58667faabf99037
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-17 13:00:07 +0200

    added style to headerComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-       header
-    </div>
+    <header>
+        <div class="container d-flex justify-content-between py-4">
+            <div class="left">
+                <h1>Boolflix</h1>
+            </div>
+            <div class="input-group mb-3 right">
+                <input type="text" class="form-control" placeholder="Cerca">
+                <button class="btn btn-outline-danger" type="button" id="buttonSearch">Cerca</button>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -11,5 +19,22 @@
 </script>
 
 <style lang="scss" scoped>
+header{
+    background-color: black;
+}
+.left{
+    width: 40%;
+    h1{
+        color: red;
+        font-weight: bolder;
+    }
+};
+.right{
+    width: 500px;
+    button{
+        color:white;
+        background-color: red;
+    }
+}
 
 </style>
